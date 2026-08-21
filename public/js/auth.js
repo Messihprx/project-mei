58afba029c55882c7f8a7dfff8a415047eb9de43
Messihprx <brenomessisilva@gmail.com>
2026-08-20 23:56:30 -0300

    fix login google

--- public/js/auth.js
+++ public/js/auth.js
@@ -407,7 +407,7 @@ const loginGoogle = async () => {
     const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: { 
-            redirectTo: window.location.origin + '/public/index.html',
+            redirectTo: window.location.origin + '/index.html',
             queryParams: {
                 access_type: 'offline',
                 prompt: 'select_account',
