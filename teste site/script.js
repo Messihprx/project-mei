15bff836bcbad94699149563d5951d6d0e8e5441
Messihprx <brenomessisilva@gmail.com>
2026-08-29 20:19:05 -0300

    Remove pastas antigas site 1 e teste site

--- teste site/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("changeColor").addEventListener("click", function() {
-    document.body.style.backgroundColor = 
-        "#" + Math.floor(Math.random()*16777215).toString(16);
-});
